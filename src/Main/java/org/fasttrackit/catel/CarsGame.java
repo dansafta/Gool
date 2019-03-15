ad5a68f4331c2ea7b1c4dd8cb2735f0794d179e3
dansafta <safta_nv@yahoo.com.au>
2019-03-15 12:48:46 +0200

    15.03.2019

--- src/Main/java/org/fasttrackit/catel/CarsGame.java
+++ src/Main/java/org/fasttrackit/catel/CarsGame.java
@@ -1,3 +1,5 @@
+package org.fasttrackit.catel;
+
 import java.util.Scanner;
 
 public class CarsGame {
---
+++ src/Main/java/org/fasttrackit/lab3/RezolvareLab3.java
@@ -0,0 +1,19 @@
+package org.fasttrackit.lab3;
+
+public class RezolvareLab3 {
+    public static void main(String [] args) {
+
+
+
+
+        int value1 = 5;
+
+        int value2 = 7;
+
+        int mediaAritmetica = (value1 + value2) / 2;
+
+        System.out.println("Media aritmentica a numerelor " + value1 + " si " + value2 + " este " + mediaAritmetica);
+
+    }
+}
+
