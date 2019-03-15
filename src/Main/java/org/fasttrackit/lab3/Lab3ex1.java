aa797bb76b3655fd7135a6d8081d9189734f6d27
dansafta <safta_nv@yahoo.com.au>
2019-03-15 15:36:00 +0200

    15.03.2019

---
+++ src/Main/java/org/fasttrackit/lab3/Lab3ex1.java
@@ -0,0 +1,17 @@
+package org.fasttrackit.lab3;
+
+public class Lab3ex1 {
+    public static void main(String [] args) {
+
+
+
+
+        int value1 = 5;
+
+        int value2 = 7;
+        int mediaAritmetica = (value1 + value2) / 2;
+
+        System.out.println("Media aritmentica a numerelor " + value1 + " si " + value2 + " este " + mediaAritmetica);
+
+    }
+}
