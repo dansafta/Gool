550dae10866e76ce0a0dab9a83b8c262ef10be92
dansafta <safta_nv@yahoo.com.au>
2019-03-18 14:57:02 +0200

    18.03.2019

--- src/Main/java/org/fasttrackit/lab3/Lab3ex1.java
+++ src/Main/java/org/fasttrackit/lab3/Lab3ex1.java
@@ -1,28 +1,57 @@
 package org.fasttrackit.lab3;
 
+import java.util.Random;
 import java.util.Scanner;
 
 
 public class Lab3ex1 {
 
-            public static void main(String[] args) {
-
-                // Create scanner class object
-                Scanner in = new Scanner(System.in);
-
-                // Input two numbers from user
-                System.out.println("Enter first number :");
-                int num1 = in.nextInt();
-                System.out.println("Enter second number :");
-                int num2 = in.nextInt();
+    public static void main(String[] args) {
 
 
-                double mediaAritmetica = (num1 + num2) / 2;
+        Scanner in = new Scanner(System.in);
 
-                System.out.println("Media aritmentica a numerelor " + num1 + " si " + num2 + " este " + mediaAritmetica);
+        // Input two numbers from user
+        System.out.println("Enter first number :");
+        int num1 = in.nextInt();
+        System.out.println("Enter second number :");
+        int num2 = in.nextInt();
 
 
-            }
+        double mediaAritmetica = (num1 + num2) / 2;
+
+        System.out.println("Media aritmentica a numerelor " + num1 + " si " + num2 + " este " + mediaAritmetica);
+
+        {
+            int n, c;
+            System.out.println("Enter an integer to print it's multiplication table");
+            n = in.nextInt();
+            System.out.println("Multiplication table of " + n);
+
+            for (c = 1; c <= 10; c++)
+                System.out.println(n + "*" + c + " = " + (n * c));
         }
+        {
+            System.out.println("Enter number :");
+            int num = in.nextInt();
+            int reversed = 0;
+
+            while(num != 0) {
+                int digit = num % 10;
+                reversed = reversed * 10 + digit;
+                num /= 10;
+            }
+
+            System.out.println("Reversed Number: " + reversed);
+        }
+
+
+    }
+
+}
+
+
+
+
 
 
--- src/Main/java/org/fasttrackit/lab3/RezolvareLab3.java
+++ src/Main/java/org/fasttrackit/lab3/RezolvareLab3.java
@@ -1,18 +1,49 @@
 package org.fasttrackit.lab3;
 
+import java.util.Scanner;
+
 public class RezolvareLab3 {
-    public static void main(String [] args) {
 
+    static int n;
+    static int[] tablou;
 
+    public static void main(String[] args) {
 
 
         double value1 = 5.6;
 
         double value2 = 7.5;
-                double mediaAritmetica = (value1 + value2) / 2;
+        double mediaAritmetica = (value1 + value2) / 2;
 
         System.out.println("Media aritmentica a numerelor " + value1 + " si " + value2 + " este " + mediaAritmetica);
 
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Dati numarul n:");
+        n = in.nextInt();
+        tablou = new int[n];
+        for (int i = 0; i < n; ++i) {
+            System.out.println("Dati elementul cu nr. " + (i + 1));
+            tablou[i] = in.nextInt();
+        }
+        System.out.println("elementul maxim este " + getMax(tablou));
+
+    }
+
+    private static int getMax(int[] array) {
+        int max = array[0];
+        for (int i = 1; i < array.length; ++i) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        return max;
+
+
+
     }
 }
+
+
+
 
